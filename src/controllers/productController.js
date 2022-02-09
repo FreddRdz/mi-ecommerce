4755732f5f7afb14e5b734681ec1f0641aba0837
IsaiasMC28 <14540@virtual.utsc.edu.mx>
2022-02-08 18:32:08 -0600

    story10 incompleto

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,7 +1,23 @@
+const fetch = require("node-fetch");
+// let products = require("../database/products");
+
 module.exports = {
   findProducts: (req, res) => {},
   findProductById: (req, res) => {},
   findProductsRelatedById: (req, res) => {},
-  findProductsSuggested: (req, res) => {},
+
+  //PRODUCTOS SUGERIDOS
+  findProductsSuggested: async (req, res) => {
+    fetch("https://dhfakestore.herokuapp.com/api/products")
+      .then((response) => response.json())
+      .then((productosSugeridos) => {
+        return res.render("product", { productosSugeridos });
+      });
+    // let id = req.params.id;
+
+    // const productosSugeridos = products.find((product) => product.id == id);
+
+    // productosSugeridos = res.render("product", { product: productosSugeridos });
+  },
   findProductsMostWanted: (req, res) => {},
 };
