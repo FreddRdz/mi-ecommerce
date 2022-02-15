526abecd311103f38d5529f5412240932f33c75d
Pena Anzures Luis Guillermo <luis.pena@xpertal.com>
2022-02-15 16:45:43 -0600

    terminando historia 8 y correciones de historia 7

--- public/js/login.js
+++ public/js/login.js
@@ -6,28 +6,26 @@ window.addEventListener('load', function(){
   let contraseñaRepeat = document.querySelector("#passwordRepeat");
   let errores = [];
   let mostrarErrores = document.querySelector(".errores ul");
-  let botonFormulario = document.querySelector(".button-register")
-  let botonEntendido = this.document.querySelector(".botonMensaje")
+  let botonFormulario = document.querySelector(".button-login")
+  let body = document.querySelector("body")
+  let botonEntendido = document.querySelector(".botonMensaje")
   
-
 
   formulario.addEventListener("submit",function(event){
 
     if (email.value == ''){
     
-      errores.push("Error en el campo email");
-
-      
+      errores.push("Error en el nombre de usuario");
     }
 
     if (contraseña.value == ''){
-     
       errores.push("Error en la contraseña")
-      
     }
 
     if (errores.length > 0){
+      
       let divErrores = document.querySelector(".errores").style.display ="block";
+     
       event.preventDefault();
       
       errores.forEach(error => {
@@ -43,8 +41,6 @@ window.addEventListener('load', function(){
       let divErrores2 = document.querySelector(".errores").style.display ="none";
      })
 
-     
-  
   });
 
 });
