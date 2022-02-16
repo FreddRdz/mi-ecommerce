51ff4c69b50980408ea8068577eb92dfc247c8ba
Alfredo Rodriguez <fredd.073100@gmail.com>
2022-02-16 15:34:24 -0600

    Story 12 implementada

--- public/js/login.js
+++ public/js/login.js
@@ -1,12 +1,19 @@
 window.addEventListener("load", function () {
+  let variable = null;
   let formulario = document.querySelector("form");
   let email = document.querySelector(".username-input");
   let contraseña = document.querySelector(".user-password");
-  let contraseñaRepeat = document.querySelector("#passwordRepeat");
   let errores = [];
   let mostrarErrores = document.querySelector(".errores ul");
   let botonFormulario = document.querySelector(".button-login");
   let botonEntendido = document.querySelector(".botonMensaje");
+  let checkbox = document.querySelector(".recuerdame");
+
+  if (localStorage.nombreUsuario) {
+    let usuario = localStorage.nombreUsuario;
+    email.setAttribute("value", usuario);
+    checkbox.checked = true;
+  }
 
   formulario.addEventListener("submit", function (event) {
     if (email.value == "") {
@@ -36,5 +43,11 @@ window.addEventListener("load", function () {
       let divErrores2 = (document.querySelector(".errores").style.display =
         "none");
     });
+
+    if (checkbox.checked) {
+      localStorage.setItem("nombreUsuario", email.value);
+    } else {
+      localStorage.removeItem("nombreUsuario");
+    }
   });
 });
